df6e843449151575d4a04dfff8686a5108c43fb5
Igor Levkovets <igorekmu.levckovets@yandex.ru>
2019-12-04 17:54:28 +0300

    first commit

---
+++ BankApplication/BankLibrary/IAccount.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankLibrary
+{
+    public interface IAccount
+    {
+        //положить деньги на счет
+        void Put(decimal sum);
+        //взять со счета
+        decimal Withdraw(decimal sum);
+    }
+}
